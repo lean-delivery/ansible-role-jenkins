541cbccb95391d3e16284c559a1e9f9125df33e8
Aliaksei_Zeliashchonak <zelalexey@gmail.com>
2019-03-04 10:37:20 +0300

    master branch

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -12,3 +12,7 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+def test_jenkins(host):
+    assert host.service('jenkins').is_running
+    assert host.socket("tcp://0.0.0.0:8080").is_listening
