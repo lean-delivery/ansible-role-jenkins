add9bdf05c87046d5dc7494a4bad46ac99f301d1
Aliaksei_Zeliashchonak <zelalexey@gmail.com>
2019-03-04 10:40:20 +0300

    fixed lint

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -13,6 +13,7 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
+
 def test_jenkins(host):
     assert host.service('jenkins').is_running
     assert host.socket("tcp://0.0.0.0:8080").is_listening
